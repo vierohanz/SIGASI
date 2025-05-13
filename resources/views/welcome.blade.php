9b3f6c7f9adc6fd5175978bef4bb78551011c75f
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2025-05-13 20:11:22 +0700

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,34 +6,20 @@
     <title>Selamat Datang</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <style>
-        @keyframes fade-in {
-            0% {
-                opacity: 0;
-                transform: translateY(20px);
-            }
-
-            100% {
-                opacity: 1;
-                transform: translateY(0);
-            }
-        }
-
-        .animate-fade-in {
-            animation: fade-in 1.5s ease-out;
-        }
-    </style>
 </head>
 
-<body class="bg-gradient-to-br from-blue-800 to-indigo-900 min-h-screen flex items-center justify-center text-white">
-    <div class="text-center px-6 animate-fade-in">
-        <h1 class="text-5xl md:text-11xl font-extrabold mb-4">Selamat Datang di SIGASI</h1>
-        <p class="text-lg md:text-xl mb-8">Kelola event kamu dengan mudah dan profesional.</p>
-        <a href=""
-            class="inline-block px-8 py-3 bg-white text-indigo-800 font-semibold rounded-full shadow-lg hover:scale-105 transition-transform duration-300">
-            Mulai Sekarang
-        </a>
-    </div>
+<body class="  text-black scroll-smooth">
+    <section id="home" class="">
+        @include('sections.home')
+    </section>
+
+    <section id="maps" class="bg-blue-900 animate-fade-in">
+        @include('sections.maps')
+    </section>
+
+    <section id="customer-service" class="bg-indigo-800 animate-fade-in">
+        @include('sections.customer-service')
+    </section>
 </body>
 
 </html>
