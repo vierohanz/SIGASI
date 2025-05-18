3cbfc6ce3f6b9d9a7a3527c6cac9590af1572af1
Salwa Fitriyatunnisa <salwafitriyatunnisa@gmail.com>
2025-05-18 19:04:20 +0700

    first commit

--- resources/views/sections/home.blade.php
+++ resources/views/sections/home.blade.php
@@ -1,6 +1,6 @@
 <div class="text-center bg-yellow-50 h-screen px-6">
     <h1 class="text-5xl md:text-6xl font-extrabold mb-4">Selamat Datang di SIGASI</h1>
-    <p class="text-lg md:text-xl mb-8">Kelola event kamu dengan mudah dan profesional.</p>
+    <p class="text-lg md:text-xl mb-8">Kelola dengan mudah dan profesional.</p>
     <a href="#customer-service"
         class="inline-block px-8 py-3 bg-white text-indigo-800 font-semibold rounded-full shadow-lg hover:scale-105 transition-transform duration-300">
         Mulai Sekarang
